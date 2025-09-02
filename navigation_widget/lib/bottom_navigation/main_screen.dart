7c7a13bae5bf2dda4318d4803bd7b426dae11d57
wwwalohacampus <h850415@naver.com>
2025-09-02 12:08:15 +0900

    .

--- navigation_widget/lib/bottom_navigation/main_screen.dart
+++ navigation_widget/lib/bottom_navigation/main_screen.dart
@@ -21,7 +21,7 @@ class _MainScreenState extends State<MainScreen> {
   void _onTab(int index) {
     print("화면을 이동합니다, ${index}");
     // ⭐ setState()
-    // : StatefulWidget 에서 변겨된 state 를 반영하여 UI 를 업데이트하는 함수
+    // : StatefulWidget 에서 변경된 state 를 반영하여 UI 를 업데이트하는 함수
     setState(() {
       _selectedIndex = index;
       switch(index) {
