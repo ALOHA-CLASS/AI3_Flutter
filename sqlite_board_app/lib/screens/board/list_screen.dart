b2aa590e31064ff82374e265012259d4ea79bd1f
wwwalohacampus <h850415@naver.com>
2025-09-03 16:41:08 +0900

    SQLite 3

--- sqlite_board_app/lib/screens/board/list_screen.dart
+++ sqlite_board_app/lib/screens/board/list_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sqlite_board_app/models/boards.dart';
+import 'package:sqlite_board_app/service/board_service.dart';
 
 class ListScreen extends StatefulWidget {
   const ListScreen({super.key});
@@ -8,8 +10,66 @@ class ListScreen extends StatefulWidget {
 }
 
 class _ListScreenState extends State<ListScreen> {
+  // 🧊 state
+  late Future<List<Map<String, dynamic>>> _boardList;
+  final boardService = BoardService();
+
+  @override
+  void initState() {
+    super.initState();
+    // 게시글 목록 요청
+    _boardList = boardService.list();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text("게시글 목록")),
+      body: 
+        Container(
+          padding: const EdgeInsets.fromLTRB(5, 0, 5, 10),
+          child: 
+            FutureBuilder(
+              future: _boardList,     // 비동기 데이터 
+              builder: (context, snapshot) {
+                // 데이터 로딩 중
+                if( snapshot.connectionState == ConnectionState.waiting ) {
+                  return const Center(child: CircularProgressIndicator(),);
+                }
+                // 에러 발생
+                else if( snapshot.hasError ) {
+                  return Column(
+                    children: [
+                      Center(child: Text("데이터 조회 시, 에러"),),
+                      Center(child: Text('Error: ${snapshot.error}'),),
+                    ],
+                  );
+                }
+                // 데이터 없음
+                else if( !snapshot.hasError && snapshot.data!.isEmpty ) {
+                  return const Center(child: Text("조회된 데이터가 없습니다."),);
+                }
+                // 데이터 있음
+                else {
+                  List<Map<String, dynamic>> boardData = snapshot.data!;
+                  return ListView.builder(
+                    itemCount: boardData.length,
+                    itemBuilder: (context, index) {
+                      final board = Boards.fromMap(boardData[index]);
+                      return Card(
+                        child: ListTile(
+                            title: Text(board.title ?? ''),
+                            subtitle: Text(board.content ?? ''),
+                          ),
+                      );
+                    }
+                  );
+                }
+              }
+            )
+        )
+    );
+
+
   }
 }
--- sqlite_board_app/lib/screens/main_screen.dart
+++ sqlite_board_app/lib/screens/main_screen.dart
@@ -10,6 +10,20 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text("메인 화면"),),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/board/list');
+          }, 
+          child: Text("게시글 목록"),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blueAccent,
+            foregroundColor: Colors.white
+          )
+        ),
+      ),
+    );
   }
 }
